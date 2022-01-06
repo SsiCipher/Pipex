75161ad1a67689772cddf953a6c1b11c4271e4e7
SsiCipher <ssicipher@mail.com>
2022-01-06 21:23:06 +0100

    ADDED: Bonus part (multiple pipes)

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 02:02:07 by yanab             #+#    #+#             */
-/*   Updated: 2022/01/05 17:50:33 by yanab            ###   ########.fr       */
+/*   Updated: 2022/01/05 17:52:03 by yanab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	cmd2_handler(int *pipe_ends, char *outfile_name, char *cmd, char **envp)
 
 void	handle_exit(pid_t proc_pid)
 {
-	int proc_status;
+	int	proc_status;
 
 	(void)proc_pid;
 	waitpid(proc_pid, &proc_status, 0);
@@ -56,31 +56,20 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc != 5)
 		print_error("Usage: ./pipex infile cmd1 cmd2 outfile\n");
-
 	pipe(pipe_ends);
-
 	cmd1_proc_pid = fork();
 	if (cmd1_proc_pid == -1)
 		print_error("Error: Failes to create a child process for CMD_1\n");
 	else if (cmd1_proc_pid == 0)
-	{
 		cmd1_handler(pipe_ends, argv[1], argv[2], envp);
-		dprintf(2, "cmd1\n");
-	}
 	else
 		handle_exit(cmd1_proc_pid);
-
 	cmd2_proc_pid = fork();
 	if (cmd2_proc_pid == -1)
 		print_error("Error: Failes to create a child process for CMD_2\n");
 	else if (cmd2_proc_pid == 0)
-	{
 		cmd2_handler(pipe_ends, argv[4], argv[3], envp);
-		dprintf(2, "cmd2\n");
-	}
-
 	close(pipe_ends[0]);
 	close(pipe_ends[1]);
-
 	return (0);
 }
--- test.c
+++ test.c
@@ -1,14 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 17:52:17 by yanab             #+#    #+#             */
-/*   Updated: 2022/01/05 19:07:42 by yanab            ###   ########.fr       */
+/*   Updated: 2022/01/05 19:48:13 by yanab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// < infile grep are | wc -l | xargs > outfile
+// ./a.out infile "grep are" "wc -l" "xargs" outfile
 
 #include "pipex.h"
 
@@ -30,37 +33,37 @@ int	main(int argc, char **argv, char **envp)
 		cmd_proc_pid = fork();
 		if (cmd_proc_pid == -1)
 			print_error("Error: The fork function failed\n");
-		else if (cmd_proc_pid == 0 && i == 0)
+		else if (cmd_proc_pid == 0)
 		{
-			infile_fd = open(argv[1], O_RDONLY);
-			if (infile_fd == -1)
-				print_error("Error: Failed to open infile\n");
-			
-			close(pipe_ends[0]);
-			dup2(infile_fd, STDIN_FILENO);
-			dup2(pipe_ends[1], STDOUT_FILENO);
-			execute_cmd(argv[i + 2], envp);
+			if (i == 0)
+			{
+				infile_fd = open(argv[1], O_RDONLY);
+				if (infile_fd == -1)
+					print_error("Error: Failed to open infile\n");
+				close(pipe_ends[0]);
+				dup2(infile_fd, STDIN_FILENO);
+				dup2(pipe_ends[1], STDOUT_FILENO);
+				close(pipe_ends[1]);
+				execute_cmd(argv[i + 2], envp);
+			}
+			else if (i == argc - 4)
+			{
+				outfile_fd = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 00666);
+				if (outfile_fd == -1)
+					print_error("Error: Failed to open outfile\n");
+				close(pipe_ends[1]);
+				dup2(pipe_ends[0], STDIN_FILENO);
+				// dup2(outfile_fd, STDOUT_FILENO);
+				close(pipe_ends[0]);
+				execute_cmd(argv[i + 2], envp);
+			}
+			else
+			{
+				dup2(pipe_ends[0], STDIN_FILENO);
+				// dup2(pipe_ends[1], STDOUT_FILENO);
+				execute_cmd(argv[i + 2], envp);
+			}
 		}
-		else if (cmd_proc_pid == 0 && i != argc - 4)
-		{
-			outfile_fd = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 00666);
-			if (outfile_fd == -1)
-				print_error("Error: Failed to open outfile\n");
-			close(pipe_ends[1]);
-			dup2(pipe_ends[0], STDIN_FILENO);
-			dup2(outfile_fd, STDOUT_FILENO);
-			execute_cmd(argv[i + 2], envp);
-		}
-		else if (cmd_proc_pid == 0 && i != 0)
-		{
-			dup2(pipe_ends[0], STDIN_FILENO);
-			dup2(pipe_ends[1], STDOUT_FILENO);
-			execute_cmd(argv[i + 2], envp);
-		}
-		// else
-		// {
-		// 	dprintf(2, "parent\n");
-		// }
 	}
 	close(pipe_ends[0]);
 	close(pipe_ends[1]);
