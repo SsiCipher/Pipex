d88d111a5ff6e06f42064e5043319825d3b93273
SsiCipher <ssicipher@mail.com>
2022-01-07 17:28:52 +0100

    ADDED: test

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 17:52:17 by yanab             #+#    #+#             */
-/*   Updated: 2022/01/05 19:07:42 by yanab            ###   ########.fr       */
+/*   Updated: 2022/01/07 03:34:18 by yanab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,37 +30,37 @@ int	main(int argc, char **argv, char **envp)
 		cmd_proc_pid = fork();
 		if (cmd_proc_pid == -1)
 			print_error("Error: The fork function failed\n");
-		else if (cmd_proc_pid == 0 && i == 0)
+		else if (cmd_proc_pid == 0)
 		{
-			infile_fd = open(argv[1], O_RDONLY);
-			if (infile_fd == -1)
-				print_error("Error: Failed to open infile\n");
-			
-			close(pipe_ends[0]);
-			dup2(infile_fd, STDIN_FILENO);
-			dup2(pipe_ends[1], STDOUT_FILENO);
+			if (i == 0)
+			{
+				infile_fd = open(argv[1], O_RDONLY);
+				if (infile_fd == -1)
+					print_error("Error: Failed to open infile\n");
+				close(pipe_ends[0]);
+				dup2(infile_fd, STDIN_FILENO);
+				dup2(pipe_ends[STDOUT_FILENO], STDOUT_FILENO);
+				close(pipe_ends[STDOUT_FILENO]);
+			}
+			else if (i != argc - 4)
+			{
+				dup2(pipe_ends[0], STDIN_FILENO);
+				close(pipe_ends[1]);
+				close(pipe_ends[0]);
+				// dup2(pipe_ends[1], STDOUT_FILENO);
+			}
+			else
+			{
+				outfile_fd = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 00666);
+				if (outfile_fd == -1)
+					print_error("Error: Failed to open outfile\n");
+				close(pipe_ends[STDOUT_FILENO]);
+				dup2(pipe_ends[STDIN_FILENO], STDIN_FILENO);
+				// dup2(outfile_fd, STDOUT_FILENO);
+				close(pipe_ends[STDIN_FILENO]);
+			}
 			execute_cmd(argv[i + 2], envp);
 		}
-		else if (cmd_proc_pid == 0 && i != argc - 4)
-		{
-			outfile_fd = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 00666);
-			if (outfile_fd == -1)
-				print_error("Error: Failed to open outfile\n");
-			close(pipe_ends[1]);
-			dup2(pipe_ends[0], STDIN_FILENO);
-			dup2(outfile_fd, STDOUT_FILENO);
-			execute_cmd(argv[i + 2], envp);
-		}
-		else if (cmd_proc_pid == 0 && i != 0)
-		{
-			dup2(pipe_ends[0], STDIN_FILENO);
-			dup2(pipe_ends[1], STDOUT_FILENO);
-			execute_cmd(argv[i + 2], envp);
-		}
-		// else
-		// {
-		// 	dprintf(2, "parent\n");
-		// }
 	}
 	close(pipe_ends[0]);
 	close(pipe_ends[1]);
